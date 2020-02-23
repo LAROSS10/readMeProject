32e28234334ef834da003a96f77dc3e6ece32afe
Lori Ross <laross70@gmail.com>
2020-02-22 21:03:19 -0600

    added majority of code

---
+++ index.js
@@ -0,0 +1,77 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+const axios = require('axios');
+
+// function promptUser() {
+    inquirer.prompt([
+        
+      {
+        type: "input",
+        name: "github",
+        message: "Enter your GitHub Username."
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Please provide a title for this readMe document?"
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please provide a description for this readMe document?"
+      }, 
+      
+      {
+        type: "input",
+        name: "installation",
+        message: "Please provide a description for how your app is installed?"
+      },         
+      {
+        type: "input",
+        name: "usage",
+        message: "Please provide a description for how your app is used?"
+      }, 
+      {
+        type: "input",
+        name: "license",
+        message: "Please provide your license information."
+      },  
+      {
+        type: "input",
+        name: "contribution",
+        message: "Please provide contribution information."
+      },       
+      {
+        type: "input",
+        name: "linkedin",
+        message: "Enter your LinkedIn URL."
+      }
+    ])
+    .then(function(info) {
+      let gitHubUserName = info.github
+      let title = info.title;
+      let description = info.description;
+      let installation = info.installation;
+      let usage = info.usage;
+      let license = info.license;
+      let contribution = info.contribution;
+      let linkedinUserName = info.linkedin
+     
+
+
+      const queryUrl = `https://api.github.com/users/${gitHubUserName}?client_id=115d68ff1497acbd665d&client_secret=aa009bdc3302e0edf821d60cd61be28008578e73`;
+      axios.get(queryUrl).then(function(resp){  
+       let email = resp.data.email
+      //  let badge = [![Generic badge](https://img.shields.io/badge/<SUBJECT>-<STATUS>-<COLOR>.svg)](https://shields.io/)
+       
+        let markDown = `# Your Project Title:\n ${title} + \n ## Description: \n\n ${description}  \n\n\n\n\n   ## Table of Contents: \n    *Installation \n    *Usage \n    *License \n    *Contribution \n\n## Installation: \n\n## Usage: \n\n## License: \n\n## Contribution \n\n## LinkedIn URL: \n\n\n\n##Email Address: \n ${email} \n## Badge:`
+
+        fs.writeFile("mynewReadme.md",markDown, function(err){
+
+        })
+      })
+
+    })
+    
+
---
+++ mynewReadme.md
@@ -0,0 +1,30 @@
+# Your Project Title:
+ gggggggggggg + 
+ ## Description: 
+
+ gggggggggggggg  
+
+
+
+
+   ## Table of Contents: 
+    *Installation 
+    *Usage 
+    *License 
+    *Contribution 
+
+## Installation: 
+
+## Usage: 
+
+## License: 
+
+## Contribution 
+
+## LinkedIn URL: 
+
+
+
+##Email Address: 
+ null 
+## Badge:
